82aa8277b783a830b85f676bf71e65cdd1d3e322
Patrick Tappe <pattappe@gmail.com>
2020-10-11 23:15:32 -0400

    4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,25 @@
 const dummy = (blogs) => {
+    console.log(blogs)
     return 1
 }
 
+const totalLikes = (blogs) => {
+    
+
+    const likes = blogs.map((el) => {
+        return el.likes
+    })
+
+    const reducer = (sum, likes) => {
+        return sum + likes
+    }
+
+    return blogs.length === 1
+        ? blogs[0].likes
+        : likes.reduce(reducer, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
